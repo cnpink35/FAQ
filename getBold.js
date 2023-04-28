0fdbedb31624b465592961d2c37c8a81df3fd11e
cnpink35 <33958963+cnpink35@users.noreply.github.com>
2023-04-28 12:44:47 +0900

    Update getBold.js

--- getBold.js
+++ getBold.js
@@ -1,3 +1,11 @@
 function getBold(){
   $("#qCont").hover{
-    
+    function(){
+      $(this).css("font-weight", "bold");
+      $(this).css("cursor", "pointer");
+    }
+    function(){
+      $(this).css("font-weight", "normal");
+    }
+  }
+}
