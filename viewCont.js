abacc037279a9f9ec62bd4f31c9a6d62e379e17f
cnpink35 <33958963+cnpink35@users.noreply.github.com>
2023-04-28 09:21:58 +0900

    Update viewCont.js

--- viewCont.js
+++ viewCont.js
@@ -3,4 +3,13 @@ show contents if you click the arrow
 created by daeun yoo on 2023.04.27
 */
 
-function 
+function view() {
+  var answer = document.getElementById('answer');
+  var status = answer.style.display;
+  
+  if(status !== 'none'){
+    status = 'none';
+  } else {
+    status = 'block';
+  }
+}
