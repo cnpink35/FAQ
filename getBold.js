97e55a3153b67e4ca93908f52cdf1abdb869e1f4
cnpink35 <33958963+cnpink35@users.noreply.github.com>
2023-04-28 13:00:14 +0900

    Update getBold.js

--- getBold.js
+++ getBold.js
@@ -1,11 +1,18 @@
+/*
+make letters bold if mouse hover
+created by daeun yoo on 2023.04.28
+*/
+
 function getBold(){
   $("#qCont").hover{
     function(){
       $(this).css("font-weight", "bold");
       $(this).css("cursor", "pointer");
+      $(this).css("color", "black");
     }
     function(){
       $(this).css("font-weight", "normal");
+      $(this).css("color", "black");
     }
   }
 }
