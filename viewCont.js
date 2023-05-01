3897f815b6403f8189e46fd96cac07ddc0329d3f
cnpink35 <33958963+cnpink35@users.noreply.github.com>
2023-05-01 20:07:08 +0900

    Add files via upload

--- viewCont.js
+++ viewCont.js
@@ -16,22 +16,30 @@ created by daeun yoo on 2023.04.27
 */
 
 function view(element) {
-            var before = document.getElementsByClassName("active")[0]               
+            var before = document.getElementsByClassName("active")[0];     
+            var q = $('.qMark img');
+            var arrow = $('.click img');
             if (before && document.getElementsByClassName("active")[0] != element) {  
                 before.nextElementSibling.style.display = 'none';
-                before.classList.remove("active");                  
+                q.attr('src', q.attr('src').replace('_op.png', '.png'));
+                arrow.attr('src', arrow.attr('src').replace('up_arrow.jpg', 'down_arrow.png'));
+                before.classList.remove("active");
             }
             element.classList.toggle("active");      
 
             var content = element.nextElementSibling;
             if (content.style.display != 'none') {      
                 content.style.display = 'none';
+                q.attr('src', q.attr('src').replace('_op.png', '.png'));
+                arrow.attr('src', arrow.attr('src').replace('up_arrow.jpg', 'down_arrow.png'));
               /*jQuery(this).find(".qMark").find("img").attr("src", jQuery(this).find(".qMark").find("img").attr("src").replace("_op.png", ".png"));
               jQuery(this).find(".click").find("img").attr("src", jQuery(this).find(".click").find("img").attr("src").replace("up_arrow.jpg", "down_arrow.png"));
               document.getElementsByClassName("qMark").src="q_mark.png";
               document.getElementsByClassName("click").src="down_arrow.png";*/
             } else {
                 content.style.display = 'block';
+                q.attr('src', q.attr('src').replace('.png', '_op.png'));
+                arrow.attr('src', arrow.attr('src').replace('down_arrow.png', 'up_arrow.jpg'));
               /*jQuery(this).find(".qMark").find("img").attr("src", jQuery(this).find(".qMark").find("img").attr("src").replace(".png", "_op.png"));
               jQuery(this).find(".click").find("img").attr("src", jQuery(this).find(".click").find("img").attr("src").replace("down_arrow.png", "up_arrow.jpg"));
               document.getElementsByClassName("qMark").src="q_mark_op.png";
